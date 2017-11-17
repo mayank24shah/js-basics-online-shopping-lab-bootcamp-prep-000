6822e763aa98e3e9472feed8b3fc18552b58d48b
Mayank Shah <mayank.24shah@gmail.com>
2017-11-17 21:09:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,20 +19,19 @@ function addToCart(item) {
 }
 
 function viewCart() {
-  // write your code here
-  if(cart.length===0){
-    console.log('Your shopping cart is empty.')
-  }
-  else{
-    var items = [];
-    for( var i =0; cart.length>i;i++){
-       for(var item in cart[i]){
-       items.push(item + "at $"+ cart[i][item])
+  if (cart.length === 0) {
+     console.log("Your shopping cart is empty.")
+  } else {
+     var items = [];
+    for (var i = 0; i < cart.length; i++) {
+       for (var item in cart[i]) {
+         items.push(item + " at $" + cart[i][item])
        }
-    }
-    console.log("In your cart, you have " + items.join(", ") + ".");
-  }
-}
+     }
+     console.log("In your cart, you have " + items.join(", ") + ".");
+   }
+ }
+
 
 function total() {
   // write your code here
