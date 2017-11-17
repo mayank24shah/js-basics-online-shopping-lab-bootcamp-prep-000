5d9529a438fdb76ad70f2b9a2fd1bd0633a99f72
Mayank Shah <mayank.24shah@gmail.com>
2017-11-17 21:07:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,28 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
+ var itemObj = {};
+ var price = Math.floor(Math.random() * 100) + 1;
+  itemObj[item] = price
+  cart.push(itemObj)
+ console.log(item+ " has been added to your cart.");
+ return cart;
 }
 
 function viewCart() {
   // write your code here
+  if(cart.length===0){
+    console.log('Your shopping cart is empty.')
+  }
+  else{
+    var items = [];
+    for( var i =0; cart.length>i;i++){
+       for(var item in cart[i]){
+       items.push(item + "at $"+ cart[i][item])
+       }
+    }
+    console.log("In your cart, you have " + items.join(", ") + ".");
+  }
 }
 
 function total() {
